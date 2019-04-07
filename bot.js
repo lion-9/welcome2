0d24f18a20c288ec1c97fdb32fc49fdc023133fb
lion-9 <46408833+lion-9@users.noreply.github.com>
2019-04-07 03:30:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ Nwaf.on('ready', () => {
   console.log(`Logged in as ${Nwaf.user.tag}!`);
 
 Nwaf.on('guildMemberAdd', member => {
-const Nwaf= member.guild.channels.get("532997721305382912");
+const Nwaf= member.guild.channels.get("535229239306354698");
 if(!Nwaf) return;
 if(Nwaf) {
-setTimeout(() => Nwaf.send("Welcome To **Ruthless** Server ..."), 5550)
+setTimeout(() => Nwaf.send("** Welcome To Mlk♚ **"), 5550)
 }
 })
 });
